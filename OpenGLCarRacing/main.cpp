c3640f4435731ec9bfebb0d9ce217a4804b32089
Szymon Wolarz <szymon.wol@vp.pl>
2017-04-28 23:16:17 +0200

    Initial commit Created OpenGL HelloWorld project.

---
+++ OpenGLCarRacing/main.cpp
@@ -0,0 +1,53 @@
+#include "GL/glew.h"
+#include "GL/glut.h"
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtc/type_ptr.hpp"
+#include <cstdio>
+
+const char* windowTitle = "Super Ultra Car Racing";
+const int windowWidth = 800;
+const int windowHeight = 600;
+const int windowPositionX = 300;
+const int windowPositionY = 100;
+
+void displayFrame() {
+
+	glClearColor(1, 0, 1, 1);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glutSwapBuffers();
+}
+
+void initializeGLUT(int *pargc, char* argv[]) {
+
+	glutInit(pargc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+
+	glutInitWindowPosition(windowPositionX, windowPositionY);
+	glutInitWindowSize(windowWidth, windowHeight);
+	glutCreateWindow(windowTitle);
+
+	glutDisplayFunc(displayFrame);
+}
+
+void initializeGLEW() {
+
+	GLenum err = glewInit();
+
+	if (err != GLEW_OK) {
+
+		fprintf(stderr, "Error: %s\n", glewGetErrorString(err));
+		exit(1);
+	}
+
+	fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+}
+
+int main(int argc, char* argv[]) {
+
+	initializeGLUT(&argc, argv);
+	initializeGLEW();
+
+	glutMainLoop();
+}
