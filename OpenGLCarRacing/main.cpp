b667cab0f30c6516f26144fdae04054c16f15b46
Marcin Leśnierowski <m.lesnierowski@gmail.com>
2017-08-09 07:57:29 +0200

    update1
    - models can be loaded
    - .dll file moved so assimp can be used
    - main.cpp from tutorial added as main_tutorialOpenGl.cpp
    - Shader.h fixed

--- OpenGLCarRacing/main.cpp
+++ OpenGLCarRacing/main.cpp
@@ -5,12 +5,21 @@
 #include "glm/gtc/type_ptr.hpp"
 #include <cstdio>
 
+// GL includes
+#include "Shader.h"
+#include "Camera.h"
+#include "Model.h"
+
+#include "SOIL2/SOIL2.h"
 
 const char* windowTitle = "Super Ultra Car Racing";
 const int windowWidth = 800;
 const int windowHeight = 600;
 const int windowPositionX = 300;
 const int windowPositionY = 100;
+
+// Camera
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 
 void displayFrame() {
 
@@ -45,10 +54,18 @@ void initializeGLEW() {
 	fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
 }
 
+
 int main(int argc, char* argv[]) {
 
 	initializeGLUT(&argc, argv);
 	initializeGLEW();
 
+
+	Shader shader("res/shaders/modelLoading.vs", "res/shaders/modelLoading.frag");
+	// Load models
+	Model ourModel("res/models/nanosuit.obj");
+
+
 	glutMainLoop();
+
 }
